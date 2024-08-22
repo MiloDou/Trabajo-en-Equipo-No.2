65a1605782bd7e5ddb58506186ebeab07ff6a4d3
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-08-22 17:57:54 -0600

    Agregar archivos de proyecto.

---
+++ Trabajo en Equipo No.2/Program.cs
@@ -0,0 +1,1 @@
+﻿//Parte de Fer
---
+++ Trabajo en Equipo No.2/Tarjeta de Débito.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_en_Equipo_No._2
+{
+    internal class Tarjeta_de_Débito
+    {
+    }
+}
